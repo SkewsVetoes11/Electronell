827cd7b19ea90278e3b554513c00a14f0702fc15
SkewsVetoes11 <skews_vetoes_0t@icloud.com>
2024-06-11 18:49:32 -0400

    tauri integration

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import CueBox from './components/CueBox.jsx'
 import TitleBox from './components/TitleBox.jsx'
-import downloadjs from 'downloadjs';
-import html2canvas from 'html2canvas';
+
 
 import './index.css'
-const handleCaptureClick = async () => {
-    const canvas = await html2canvas(document.querySelector('.captureArea'));
-    const dataURL = canvas.toDataURL('image/png');
-    downloadjs(dataURL, 'download.png', 'image/png');
-};
+
 
 // ...
 
@@ -28,6 +23,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         </div>
         <TitleBox type={"Summary"}/>
         </div>
-        <button onClick={handleCaptureClick}>Export as image</button>
     </React.StrictMode>
 )
