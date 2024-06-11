f32c7e613bd89e09b494c5c059e18a45b4fd8e47
SkewsVetoes11 <skews_vetoes_0t@icloud.com>
2024-06-11 17:50:36 -0400

    allows to save as image

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,31 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import CueBox from './components/CueBox.jsx'
 import TitleBox from './components/TitleBox.jsx'
+import downloadjs from 'downloadjs';
+import html2canvas from 'html2canvas';
 
 import './index.css'
+const handleCaptureClick = async () => {
+    const canvas = await html2canvas(document.querySelector('.captureArea'));
+    const dataURL = canvas.toDataURL('image/png');
+    downloadjs(dataURL, 'download.png', 'image/png');
+};
+
+// ...
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <TitleBox type={"Title"}/>
-      <div className="main-area">
+    <React.StrictMode>
+        <div className={"captureArea"}>
+        <TitleBox type={"Title"}/>
+        <div className="main-area">
 
-          <CueBox/>
+            <CueBox/>
 
-          <App/>
-      </div>
-      <TitleBox type={"Summary"}/>
-  </React.StrictMode>,
+            <App/>
+        </div>
+        <TitleBox type={"Summary"}/>
+        </div>
+        <button onClick={handleCaptureClick}>Export as image</button>
+    </React.StrictMode>
 )
