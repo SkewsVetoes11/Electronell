cce137ae5e9dd39e1da8c29b788c1d6218997ed8
SkewsVetoes11 <skews_vetoes_0t@icloud.com>
2024-06-11 11:10:34 -0400

    webver init

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import {useRef} from 'react';
+import {Editor} from '@tinymce/tinymce-react';
+// import './App.css';
+
+export default function App() {
+    const editorRef = useRef(null);
+    // const log = () => {
+    //     if (editorRef.current) {
+    //         console.log(editorRef.current.getContent());
+    //     }
+    // };
+
+    return (
+        <>
+            <div id={"NotesBox"}>
+                <Editor
+                    tinymceScriptSrc='/tinymce/tinymce.min.js'
+                    licenseKey='GPL'
+                    onInit={(_evt, editor) => editorRef.current = editor}
+                    initialValue='<p>Notes go to here... </p>'
+                    init={{
+                        height: "500px",
+                        width:"700px",
+                        selector: 'div.tinymce',
+                        plugins: [ 'quickbars' ],
+                        toolbar: false,
+                        menubar: false,
+                        inline: true,
+                        border: "solid 1px"
+                    }}
+                />
+            </div>
+
+        </>
+    );
+}
