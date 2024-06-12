4fce5f32fdb896141fadd6f6bbfa60f58221949f
Ricky Lu <ricky_lu0@outlook.com>
2024-06-11 20:11:01 -0400

    implemented some menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,30 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
 
 fn main() {
+
+  let save_as_image = CustomMenuItem::new("save_as_image".to_string(), "Save As Image...");
+  let about_ernell = CustomMenuItem::new("about_ernell".to_string(), "About Ernell");
+  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+
+  let submenu = Submenu::new("File", Menu::new().add_item(save_as_image).add_item(quit));
+  let about_submenu = Submenu::new("Help", Menu::new().add_item(about_ernell));
+
+  let menu = Menu::new()
+  .add_submenu(submenu).add_submenu(about_submenu);
   tauri::Builder::default()
+    .menu(menu)
+    .on_menu_event(|event| {
+      match event.menu_item_id() {
+        "quit" => {
+          std::process::exit(0);
+        }
+        "close" => {
+          event.window().close().unwrap();
+        }
+        _ => {}
+      }})
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
