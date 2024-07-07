0bf84566cd7c8ea01fe0887b375be1d54ea0f61d
SkewsVetoes11 <skews_vetoes_0t@icloud.com>
2024-07-07 00:32:52 -0400

    Adds support to export note as PDF (Text Selectable)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import NotesBox from "./components/NotesBox.jsx";
-
 import CueBox from "./components/CueBox.jsx";
 import TitleBox from "./components/TitleBox.jsx";
 import downloadjs from "downloadjs";
 import html2canvas from "html2canvas";
-
+import {useRef} from "react";
+import {useReactToPrint} from 'react-to-print'
 export default function App() {
   const handleCaptureClick = async () => {
     const canvas = await html2canvas(document.querySelector(".captureArea"));
@@ -17,11 +17,16 @@ export default function App() {
   const handleHideHints = () => {
     document.querySelector("#hints").style.visibility = "hidden";
   };
+
+    const componentRef = useRef();
+    const handlePrint = useReactToPrint({
+        content: () => componentRef.current
+    });
   // ...
 
   return (
     <>
-      <div className={"captureArea"}>
+      <div ref={componentRef} className={"captureArea"}>
         <TitleBox type={"Title"} />
         <div className="main-area">
           <CueBox />
@@ -33,6 +38,10 @@ export default function App() {
       <button className={"button-1"} onClick={handleCaptureClick}>
         Export as image
       </button>
+      <button className="button-2" onClick={handlePrint}>
+        Export as PDF
+        </button>
+
       <br></br>
       <br></br>
 
